6867672de8ad2a30c32526e750369a211aaeabd8
tommaso-biolo <tommasobiolo98@gmail.com>
2024-09-11 16:27:30 +0200

    Initial Commit

---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,23 @@
+import { formatCurrency } from '../../utils/helpers';
+
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li className="py-3 space-y-1">
+      <div className="flex items-center justify-between gap-4 text-sm">
+        <p>
+          <span className="font-bold">{quantity}&times;</span> {name}
+        </p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
+      </div>
+      <p className='capitalize italic text-stone-500 text-sm'>
+        {
+          isLoadingIngredients ? 'Loading...' : ingredients.join(', ')
+        }
+      </p>
+    </li>
+  );
+}
+
+export default OrderItem;
